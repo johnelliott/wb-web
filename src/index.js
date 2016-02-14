2904661588a5fe94794b7eda99fd4101e060f3f2
John Elliott <johnelliott703@gmail.com>
2016-02-14 01:47:04 -0500

    Use Redux to manage app state

--- src/index.js
+++ src/index.js
@@ -1,47 +1,90 @@
+var Redux = require('redux');
 var PouchDB = require('pouchdb');
 var db = new PouchDB('hits');
 var remoteCouch = 'http://localhost:5984/data';
 
 var view = document.getElementById('app');
 
+/*
+ * Redux action creators
+ *
+ * Use string literals for action types, no constants for actions
+ * Use like: dispatch(addhit(hit));
+ */
+function addHit (hit) {
+  return {
+    type: 'ADD_HIT',
+    hit // ES6 shorthand
+  };
+}
+
+var initialState = {
+    hits: []
+};
+
+// Redux reducer
+function counterApp (state, action) {
+    if (typeof state === 'undefined') {
+        return initialState;
+    }
+    switch (action.type) {
+        case 'ADD_HIT':
+            return Object.assign({}, state, {
+                hits: action.hit
+            });
+        default:
+            return state;
+    }
+}
+
+// Redux store
+var store = Redux.createStore(counterApp);
+store.subscribe(function reduxUIUpdate () {
+    var row = document.createElement('li');
+    row.innerHTML = JSON.stringify(store.getState());
+    view.appendChild(row);
+});
+
+
+
+// PouchDB stuff
+db.changes({
+    since: 'now',
+    live: true
+}).on('change', fetchDocs);
+
+// Local data store methods
 function sync () {
     console.log('syncing');
     var opts = {live: true};
     //db.replicate.to(remoteCouch, opts, syncError);
     db.replicate.from(remoteCouch, opts, syncError);
 }
-
-db.changes({
-  since: 'now',
-  live: true
-}).on('change', showTodos);
-
-function showTodos() {
-  db.allDocs({include_docs: false, descending: true}, function(err, doc) {
-      updateUI(doc.rows);
+function fetchDocs() {
+    db.allDocs({include_docs: false, descending: true}, function(err, doc) {
+        updateUI(doc.rows);
     });
 }
-
-function updateUI (data) {
-    data.forEach(function(doc) {
-        // fetch from pouch
-        db.get(doc.id, appendToDOM);
-    });
-}
-
-function appendToDOM (err, result) {
-    if (err) {
-        console.error(err);
-    }
-    var docRow = document.createElement('li');
-    docRow.innerHTML = JSON.stringify(result);
-    app.appendChild(docRow);
-}
-
-
+//TODO kill this function and change to console.error or something
 function syncError (err) {
     console.log('syncing error ' + err);
 }
 
+// View stuff
+function updateUI (data) {
+    data.forEach(function(doc) {
+        // fetch from pouch
+        db.get(doc.id, sendToRedux);
+    });
+}
+
+function sendToRedux (err, result) {
+    if (err) {
+        console.error(err);
+    }
+    store.dispatch(addHit(result));
+}
+
+// Init
 sync();
-showTodos();
+fetchDocs();
