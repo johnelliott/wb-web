45ea6cc5a93fad60577d916c49e44a7ed4858a08
John Elliott <johnelliott703@gmail.com>
2016-02-14 22:33:33 -0500

    wip-get hits count updating via react

--- src/index.js
+++ src/index.js
@@ -6,15 +6,37 @@ var db = new PouchDB('hits');
 var remoteCouch = 'http://localhost:5984/data';
 
 var view = document.getElementById('app');
-ReactDOM.render(
-    <h1>Hello, world!</h1>,
-    document.getElementById('app')
+
+/*
+ * React ================================================
+ */
+const render = () => {
+    ReactDOM.render(
+        <Counter hits={store.getState()} />,
+        document.getElementById('root')
+    );
+};
+
+/*
+ * React components
+ */
+
+/* Counter
+ * props: value <number>
+ */
+const Counter = ({ hits }) => (
+    <div>
+        <p className="subtitle">Counter component</p>
+        <p>Hits: {hits.hits}</p>
+    </div>
 );
 
+/*
+ * Redux ================================================
+ */
 function logReduxState () {
-  console.log('Redux state =============');
+  console.log('============= Redux state =============');
   console.log(JSON.stringify(store.getState()));
-  console.log('============= /Redux state');
 }
 /*
  * Redux action creators
@@ -23,17 +45,15 @@ function logReduxState () {
  * Use like: dispatch(addhit(hit));
  */
 // TODO consider Redux standard action: https://github.com/acdlite/flux-standard-action for later
-function addHit (hit) {
-  //console.log('adding hit', hit);
+function addHit () {
   return {
-    type: 'ADD_HIT',
-    hit: hit
+    type: 'ADD_HIT'
   };
 }
 
 var initialState = {
     //TODO why does this need an empty object in it to work in the reducer?
-    hits: [{}]
+    hits: 0
 };
 
 // Redux reducer
@@ -43,12 +63,10 @@ function counterApp (state, action) {
     }
     switch (action.type) {
         case 'ADD_HIT':
-            var newState = Object.assign({}, state, {
-              // probably adding dupe ids or revs...
-              // TODO find out why empty array is needed to preseve types
-              hits: state.hits.concat(action.hit)
-            });
-            console.log('newState' + newState);
+            var newState = {
+                hits: state.hits + 1
+            };
+            logReduxState();
             return newState;
         default:
             return state;
@@ -59,61 +77,50 @@ function counterApp (state, action) {
 // Create Redux store
 var store = Redux.createStore(counterApp, initialState);
 
+// PouchDB stuff
+db.changes({
+    since: 'now',
+    live: true
+}).on('change', fetchDocs);
+
+// Local data store methods
+function sync () {
+    console.log('syncing');
+    var opts = {live: true};
+    //db.replicate.to(remoteCouch, opts, syncError);
+    db.replicate.from(remoteCouch, opts, syncError);
+}
+function fetchDocs() {
+    db.allDocs({include_docs: false, descending: true}, function(err, doc) {
+        updateUI(doc.rows);
+    });
+}
+//TODO kill this function and change to console.error or something
+function syncError (err) {
+    console.log('syncing error ' + err);
+}
+
+// View stuff
+function updateUI (data) {
+    data.forEach(function(doc) {
+        // fetch from pouch
+        db.get(doc.id, sendToRedux);
+    });
+}
+
+function sendToRedux (err, result) {
+    if (err) {
+        console.error(err);
+    }
+    store.dispatch(addHit());
+}
+
+
 // Subscribe to events and update DOM
-store.subscribe(function reduxUIUpdate () {
-    var row = document.createElement('li');
-    row.innerHTML = JSON.stringify(store.getState());
-    view.appendChild(row);
-});
+store.subscribe(render);
+// Do an inital render
+render();
 
-
-/*
- * React components
- *
- *
- */
-
-
-
-
-//// PouchDB stuff
-//db.changes({
-///    since: 'now',
-//    live: true
-//}).on('change', fetchDocs);
-//
-//// Local data store methods
-//function sync () {
-//    console.log('syncing');
-//    var opts = {live: true};
-//    //db.replicate.to(remoteCouch, opts, syncError);
-//    db.replicate.from(remoteCouch, opts, syncError);
-//}
-//function fetchDocs() {
-//    db.allDocs({include_docs: false, descending: true}, function(err, doc) {
-//        updateUI(doc.rows);
-//    });
-//}
-////TODO kill this function and change to console.error or something
-//function syncError (err) {
-//    console.log('syncing error ' + err);
-//}
-//
-//// View stuff
-//function updateUI (data) {
-//    data.forEach(function(doc) {
-//        // fetch from pouch
-//        db.get(doc.id, sendToRedux);
-//    });
-//}
-//
-//function sendToRedux (err, result) {
-//    if (err) {
-//        console.error(err);
-//    }
-//    store.dispatch(addHit(result));
-//}
-//
-//// Init
-//sync();
-//fetchDocs();
+// Init
+sync();
+fetchDocs();
