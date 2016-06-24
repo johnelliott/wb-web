d91191aab2e161a7deff3fbafa509c34c08c7f0f
John Elliott <johnelliott703@gmail.com>
2016-06-24 13:31:17 -0400

    Serve compressed assets if present, add gzip'n

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
-var debug = require('debug')('wb-web:index');
-var http = require('http');
-var url = require('url');
-var ecstatic = require('ecstatic');
-var request = require('request');
+var debug = require('debug')('wb-web:index')
+var http = require('http')
+var url = require('url')
+var ecstatic = require('ecstatic')
+var request = require('request')
 
 // Middlewares
-var staticMiddleware = ecstatic({ root: __dirname + '/public' });
+var staticMiddleware = ecstatic({
+  root: __dirname + '/public',
+  gzip: true
+})
 
 // Server
 var server = http.createServer(function requestHandler (req, res) {
   if (req.url.match(/\/api/)) {
-    var urlParts = url.parse(req.url);
+    var urlParts = url.parse(req.url)
     // TODO remove hacky slice
-    var apiBase = '/api';
-    var urlResolved = url.resolve(apiBase, 'http://127.0.0.1:5984/data' + urlParts.path.slice(apiBase.length));
-    req.pipe(request(urlResolved)).pipe(res);
+    var apiBase = '/api'
+    var urlResolved = url.resolve(apiBase, 'http://127.0.0.1:5984/data' + urlParts.path.slice(apiBase.length))
+    req.pipe(request(urlResolved)).pipe(res)
   } else {
-    staticMiddleware(req, res);
+    staticMiddleware(req, res)
   }
-});
+})
 
 // Start server
-var port = process.env.PORT || 8080;
-server.listen(port);
-debug('Server listening on http://localhost:' + port);
+var port = process.env.PORT || 8080
+server.listen(port)
+debug('Server listening on http://localhost:' + port)
