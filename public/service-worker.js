ac9af8210be42bf7e639603860e500ca0f99e886
John Elliott <johnelliott703@gmail.com>
2016-06-27 12:18:59 -0400

    Cache assets in service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,45 @@
 // TODO move this file to ../src/ and update browserify to put this in place
+
+self.addEventListener('activate', function(event) {
+	console.log('Activated', event)
+})
+
+self.addEventListener('install', function(e) {
+	self.skipWaiting()
+
+  e.waitUntil(
+    caches.open('waybot-web').then(function(cache) {
+      console.log('cache opened', cache)
+      return cache.addAll([
+        '/',
+        '/index.html',
+        //'/?homescreen=1',
+        //'/index.html?homescreen=1',
+        '/tufte.css',
+        '/bundle.js',
+        '/manifest.json',
+        '/images/pi.jpg',
+        '/images/1-counter.jpg',
+        '/images/icon-144x144.png',
+        '/images/icon-192x192.png',
+        '/images/icon-256x256.png',
+        '/images/icon-384x384.png',
+        '/images/icon-512x512.png',
+        '/images/touch-icon-ipad-retina.png',
+        '/images/touch-icon-ipad.png',
+        '/images/touch-icon-iphone-retina.png',
+        '/images/touch-icon-iphone.png'
+      ])
+    })
+  )
+})
+
+self.addEventListener('fetch', function(event) {
+  //console.log('fetch event', event.request.url)
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      //console.log('SW response', response ? response : 'response is undefined?!')
+      return response || fetch(event.request)
+    })
+ )
+})
