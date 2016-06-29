f17f654d5da049ebf27b4f5ee4352435764d4c66
John Elliott <johnelliott703@gmail.com>
2016-06-28 21:32:13 -0400

    Simplify initial pouchDB -> redux flow

--- src/db.js
+++ src/db.js
@@ -1,53 +1,40 @@
-var PouchDB = require('pouchdb');
-var state = require('./state.js');
-var db = new PouchDB('hits');
+var PouchDB = require('pouchdb')
+var state = require('./state.js')
+var db = new PouchDB('hits')
 // TODO such bad, very hack....? YES document.url KILLED AN HOUR OF MY TIME
 var remoteHost = document.URL + 'api'
-var remoteCouch = new PouchDB(remoteHost);
-PouchDB.debug.enable('*');
+var remoteCouch = new PouchDB(remoteHost)
+PouchDB.debug.enable('*')
 // PouchDB stuff
 db.changes({
     since: 'now',
     live: true
-}).on('change', fetchDocs);
+}).on('change', fetchDocs)
 
 // Local data store methods
 function sync () {
-    console.log('syncing');
-    var opts = {live: true};
+    console.log('syncing')
+    var opts = {live: true}
     db.replicate.from(remoteCouch, opts, function syncErrorLogger (err) {
-          console.log('syncing error ' + err);
-    });
+      console.log('syncing error ' + err)
+    })
 }
 function fetchDocs() {
-    db.allDocs({include_docs: false, descending: true}, function(err, doc) {
+    db.allDocs({include_docs: true, descending: true}, function allDocsCB(err, doc) {
         if (err) {
-          console.error(err);
-          return err;
+          console.error(err)
+          return err
         }
-        updateUI(doc.rows);
-    });
-}
-
-// View stuff
-// TODO, call only once instead of for every hit, then have redux process the state changes appropriately
-function updateUI (data) {
-    data.forEach(function(doc) {
-        // fetch from pouch
-        db.get(doc.id, sendToRedux);
-    });
-}
-
-// TODO should this go in the redux module?
-function sendToRedux (err, result) {
-    if (err) {
-        console.error(err);
-    }
-    //TODO this is just temp because safari sucks...
-    state.store.dispatch(state.addCounter(result));
-    state.store.dispatch(state.addHit(result));
+        console.log('db:docs', doc)
+        doc.rows.forEach(row=>{
+          console.log('dispatching', row.doc)
+          //TODO this is just temp because safari sucks...
+          state.store.dispatch(state.addCounter(row.doc))
+          state.store.dispatch(state.addHit(row.doc))
+        })
+    })
 }
 
 // Init
-sync();
-fetchDocs();
+sync()
+fetchDocs()
